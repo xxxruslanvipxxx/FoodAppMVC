c95f758b4a9accf905c13f5616c91598cfbe6d12
xxxruslanvipxxx <rysfan00@gmail.com>
2023-11-22 17:59:00 +0300

    Initial Commit

---
+++ FoodAppMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodAppMVC
+//
+//  Created by Руслан Забиран on 22.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
